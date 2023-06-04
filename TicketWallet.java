69f577671cf6967ced1722eb2995132a0d5eb9eb
alfian <alfianmusthofa45264@gmail.com>
2023-06-04 14:47:52 +0700

    alfian

---
+++ TicketWallet.java
@@ -0,0 +1,27 @@
+/**
+ * TicketWallet
+ */
+public class TicketWallet {
+
+    private int nominal;
+
+    public void beliSaldo(int nominal){
+        this.nominal += nominal;
+    }
+
+    public void cekSaldo(){
+        System.out.println("Saldo anda Rp." + this.nominal);
+    }
+
+    public void bayarTiket(int nominal){
+
+        if (nominal <= this.nominal) {
+            this.nominal -= nominal;
+            System.out.println("Pembelian berhasil!");
+        }
+        
+        else {
+            System.out.println("Saldo tidak cukup!");
+        }
+    }
+}
